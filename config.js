749c4501fd063484c24e9a596d6096bbc86f5c89
souparno <souparno.majumder@gmail.com>
2019-02-23 05:31:08 +0530

    path to atom src can be set through config.json

--- config.js
+++ config.js
@@ -2,13 +2,13 @@
 
 const fs = require('fs');
 const path = require('path');
-const localConfig = require('./config.local');
-const universalConfig = require(localConfig.src, 'script/config');
-const atomInOrbitRoot = path.resolve(__dirname, '.')
-const atomInOrbitLib = path.join(atomInOrbitRoot, 'lib')
-const atomInOrbitScript = path.join(atomInOrbitRoot, 'script')
-const atomInOrbitStatic = path.join(atomInOrbitRoot, 'static')
-const atomInOrbitShims = path.join(atomInOrbitRoot, 'shims')
+const localConfig = require('./config.json');
+const universalConfig = require(path.join(localConfig.src, 'script/config'));
+const atomInOrbitRoot = path.resolve(__dirname, '.');
+const atomInOrbitLib = path.join(atomInOrbitRoot, 'lib');
+const atomInOrbitScript = path.join(atomInOrbitRoot, 'script');
+const atomInOrbitStatic = path.join(atomInOrbitRoot, 'static');
+const atomInOrbitShims = path.join(atomInOrbitRoot, 'shims');
 
 module.exports = Object.assign({
   'atomInOrbitRoot': atomInOrbitRoot,
